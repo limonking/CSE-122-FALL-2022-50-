19e9094ccbcb15afe9c4d608bdbf48efd42ee4c2
Md Mahafuzar rahman <122246149+limonking@users.noreply.github.com>
2023-02-05 23:32:11 +0600

    Add files via upload

---
+++ code hub2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n;
+    while (n--)
+    {
+        cin >> s;
+        if (s.length() > 10)
+        {
+            cout << s[0] << s.length() - 2 << s[s.length() - 1] << endl;
+        }
+        else
+        {
+            cout << s << endl;
+        }
+    }
+    getch ();
+}
