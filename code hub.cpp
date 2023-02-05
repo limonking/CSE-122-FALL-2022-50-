a9ca63e3f5e3e790d417c9f812a74608994c40ec
Md Mahafuzar rahman <122246149+limonking@users.noreply.github.com>
2023-02-05 23:17:44 +0600

    Add files via upload

---
+++ code hub.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w;
+    if(w%2==0&&w>2)
+    {
+        cout<<"yes"<<endl;
+    }
+    else{cout<<"no"<<endl;
+
+    }
+    getch ();
+}
